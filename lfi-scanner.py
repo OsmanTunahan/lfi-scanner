217f328e31f07d4f949bae1730aae9835eba4c67
OsmanTunahan <osmantunahan007@gmail.com>
2024-08-08 11:05:07 +0300

    Add output handlers

--- lfi-scanner.py
+++ lfi-scanner.py
@@ -12,3 +12,22 @@ class HTTPClient:
 
     def get(self, url):
         return requests.get(url, headers=self.headers, proxies=self.proxies, allow_redirects=False, verify=False)
+    
+
+class OutputHandler:
+    def write_output(self, lines):
+        raise NotImplementedError("Subclasses should implement this!")
+
+class FileOutputHandler(OutputHandler):
+    def __init__(self, output_file):
+        self.output_file = output_file
+
+    def write_output(self, lines):
+        with open(self.output_file, 'a') as out_file:
+            for line in lines:
+                out_file.write(line + "\n")
+
+class ConsoleOutputHandler(OutputHandler):
+    def write_output(self, lines):
+        for line in lines:
+            print(line)
